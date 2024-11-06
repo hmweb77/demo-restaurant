7d1363aba4f98879304e8cf6a7f4c240ba255858
hmweb77 <oussamahamdani1@gmail.com>
2024-11-06 17:11:19 +0000

    adding project id

--- sanity.cli.ts
+++ sanity.cli.ts
@@ -4,7 +4,7 @@
 **/
 import { defineCliConfig } from 'sanity/cli'
 
-const projectId = process.env.NEXT_PUBLIC_SANITY_PROJECT_ID
-const dataset = process.env.NEXT_PUBLIC_SANITY_DATASET
+const projectId = "quaxgg3z"
+const dataset = "restaurant"
 
 export default defineCliConfig({ api: { projectId, dataset } })
